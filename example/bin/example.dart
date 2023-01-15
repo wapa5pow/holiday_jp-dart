de6b032cd37c37462846e3fe8d4599ea164809d9
wapa5pow <wapa5pow@gmail.com>
2023-01-16 04:39:26 +0900

    add example

---
+++ example/bin/example.dart
@@ -0,0 +1,9 @@
+import 'package:holiday_jp/holiday.dart';
+
+void main(List<String> arguments) {
+  final holiday = getHoliday(DateTime(2020, 7, 23));
+  if (holiday == null) {
+    throw Exception('holiday mus not be null');
+  }
+  print('${holiday.date} is ${holiday.nameEn} in Japan.');
+}
