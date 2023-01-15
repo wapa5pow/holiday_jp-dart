3bc471c8e94735cc184035a2155ef33534b91b67
wapa5pow <wapa5pow@gmail.com>
2023-01-15 09:54:37 +0900

    initialize dart package

---
+++ bin/holiday_jp_dart.dart
@@ -0,0 +1,5 @@
+import 'package:holiday_jp_dart/holiday_jp_dart.dart' as holiday_jp_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${holiday_jp_dart.calculate()}!');
+}
---
+++ test/holiday_jp_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:holiday_jp_dart/holiday_jp_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
